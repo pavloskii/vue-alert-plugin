d76c2a781232e36402f08969f53c296277aa3d1a
Igor Pavloski <Igor.Pavloski@seavus.com>
2019-10-03 21:45:25 +0200

    First commit

---
+++ src/ReallySimpleAlert.vue
@@ -0,0 +1,21 @@
+<script>
+import { setTimeout } from "timers";
+export default {
+  name: "ReallySimpleAlert",
+  props: {
+    message: {
+      type: String,
+      default: "Hello World"
+    },
+    timeout: {
+      type: Number,
+      default: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      alert(this.message);
+    }, this.timeout);
+  }
+};
+</script>
